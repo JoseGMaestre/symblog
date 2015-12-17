ae4b78a02a6a3e9700cb7a125983a53b0182c88b
Jose Maestre <jose.maestre@linio.com>
2015-12-17 16:42:11 -0430

    El Blog funcionando....

---
+++ permisos.sh
@@ -0,0 +1,7 @@
+sudo chmod 777 /var/www/html/proyectos/symblog/app/cache/dev
+sudo chmod 777 /var/www/html/proyectos/symblog/app/cache/dev/assetic/routing.yml
+sudo chmod 777 /var/www/html/proyectos/symblog/app/cache/dev/classes.map
+sudo chmod 777 /var/www/html/proyectos/symblog/app/cache/dev/doctrine/orm/Proxies
+sudo chmod 777 /var/www/html/proyectos/symblog/app/cache/dev/annotations
+sudo mkdir /var/www/html/proyectos/symblog/app/cache/dev/assetic/config/
+sudo chmod 777  /var/www/html/proyectos/symblog/app/cache/dev/assetic/config/
--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -32,8 +32,8 @@ class PageController extends Controller {
 				$message = \Swift_Message::newInstance()
 					->setSubject('Contact enquiry from symblog')
 					->setFrom('enquiries@symblog.co.uk')
-					->setTo('email@email.com')
-					->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+					->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+				                         ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
 				$this->get('mailer')->send($message);
 
 				$this->get('session')->getFlashBag()->add('notice', 'Your changes were saved!');
